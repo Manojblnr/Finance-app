6e4824299dd8e9c77dff4435610708b9641631c4
unknown <manojblnr25@gmail.com>
2023-02-05 17:20:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Form from './components/Form';
+import Graph from './components/Graph';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
+      <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">Finance Tracker</h1>
+
+      {/* Grid coloumns */}
+      <div className="grid md:grid-cols-2 gap-4">
+        {/* chart */}
+        <Graph/>
+        <Form/>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
